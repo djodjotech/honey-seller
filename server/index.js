e3d93113148dc9e7964aa9a05cee4d647b7e24ba
djodjotech <djodjotech@gmail.com>
2024-04-12 13:19:55 +0200

    Adjust initial html page

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import express from 'express';
-import App from '../client/components/App';
+// ./server/index.js
 
-const app = express();
-const port = 3000;
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import express from 'express'
+import App from '../client/components/App'
+
+const app = express()
+const port = process.env.NODE_ENV === 'production' ? process.env.PORT : 3000; // [A]
+const cdnHost = process.env.NODE_ENV === 'production' ? `https://storage.googleapis.com/react-ssr/build/client` : `http://localhost:5000`; // [B]
 
 app.get('/', (req, res) => {
-    const jsx = ReactDOMServer.renderToString( // [A]
-        <App />
-    ); 
+    const jsx = ReactDOMServer.renderToString(<App />) 
 
-    const clientBundleScript = `<script src="http://localhost:8080/scripts/bundle.js"></script>`; // [B]
-    const clientBundleStyle = `<link rel="stylesheet" href="http://localhost:8080/styles/bundle.css">`; // [B]
+    const clientBundleStyle = `<link rel="stylesheet" href="${cdnHost}/styles/bundle.css">`
+    const clientBundleScript = `<script src="${cdnHost}/scripts/bundle.js"></script>`
 
     res.send(`
         <!DOCTYPE html>
@@ -20,17 +21,17 @@ app.get('/', (req, res) => {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Stefanov Zlatni Med</title>
-                ${clientBundleStyle} <!-- [B] -->
+                <title>My SSR App</title>
+                ${clientBundleStyle} 
             </head>
             <body>
-                <div id='ssr-app'>${jsx}</div> <!-- [A] -->
-                ${clientBundleScript} <!-- [B] -->
+                <div id='ssr-app'>${jsx}</div> 
+                ${clientBundleScript} 
             </body>
         </html>
-    `);
-});
+    `)
+})
 
 app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`);
+    console.log(`App listening on http://localhost:${port}`)
 });
